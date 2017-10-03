90403c0a7002741e63b6e47d1e9660eeeb510a78
ngocquyhoang <ngocquyhoang.93@gmail.com>
2017-10-04 06:16:26 +0700

    Fix No Sign in message on Sign in path

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -13,4 +13,12 @@ module Accessible
       redirect_to( users_dashboard_index_path ) && return
     end
   end
+
+  def check_user_has_flash
+    if current_admin
+      redirect_to( admins_dashboard_index_path ) && return
+    elsif current_user
+      redirect_to( users_dashboard_index_path ) && return
+    end
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,20 @@
 class Users::SessionsController < Devise::SessionsController
   include Accessible
-  skip_before_action :check_user, only: :destroy
-  
+  skip_before_action :check_user, only: [:destroy, :create]
+  before_action :check_user_has_flash, only: :create
+  before_action :check_user_has_flash, :if => sign_in_params.empty?, only: :new
+
   layout :user_layout
+
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    puts "sign_in_paramsxxx"
+    puts sign_in_params
+    # byebug
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    respond_with(resource, serialize_options(resource))
+  end
 
   protected
   def user_layout
