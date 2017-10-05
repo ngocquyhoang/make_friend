9a3555bd02a2a94b1c3e0289eb9e61fe83ba5a86
ngocquyhoang <ngocquyhoang.93@gmail.com>
2017-10-05 18:20:06 +0700

    Sync UI

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -6,15 +6,6 @@ module Accessible
 
   protected
   def check_user
-    # flash.clear
-    if current_admin
-      redirect_to( admins_dashboard_index_path ) && return
-    elsif current_user
-      redirect_to( users_dashboard_index_path ) && return
-    end
-  end
-
-  def check_user_has_flash
     if current_admin
       redirect_to( admins_dashboard_index_path ) && return
     elsif current_user
