9941a4f5fbe02372c2a4729b85f16e7f94813703
ngocquyhoang <ngocquyhoang.93@gmail.com>
2017-10-19 00:10:47 +0700

    Sync UI part 16

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,10 @@ module ApplicationHelper
       end
     end
   end
+
+  def get_age dob
+    return nil unless dob
+    now = Time.now
+    now.year - dob.year - ( dob.to_time.change(:year => now.year) > now ? 1 : 0 )
+  end
 end
