d534ea05dda03ff01b3e8786a51ef5235785e575
ngocquyhoang <ngocquyhoang.93@gmail.com>
2017-10-24 14:06:14 +0700

    Update user profile task

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,6 +13,32 @@ module ApplicationHelper
     end
   end
 
+  def stick_avatar_for user
+    if user.avatar?
+      avatar = cl_image_tag(@user.avatar.full_public_id, :transformation=>[{:width=>400, :height=>400, :gravity=>"auto", :radius=>"max", :crop=>"crop"}, {:width=>200, :crop=>"scale"}], class: "ui avatar image")
+    else
+      if user.gender == "male"
+        avatar = image_tag "user-avatar-male.png", class: "ui avatar image"
+      elsif user.gender == "female"
+        avatar = image_tag "user-avatar-female.png", class: "ui avatar image"
+      else
+        avatar = image_tag "user-avatar-default.png", class: "ui avatar image"
+      end
+    end
+
+    user_link_path = user.username? ? user_path(user.username) : user_path(user.id)
+    if user.name?
+      name = user.name
+    else
+      if user.username?
+        name = "@#{user.username}"
+      else 
+        name = user.email
+      end
+    end
+    return "<a href='#{user_link_path}' class='stick-avatar no-outline'>#{avatar} #{name}</a>".html_safe
+  end
+
   def get_age dob
     return nil unless dob
     now = Time.now
